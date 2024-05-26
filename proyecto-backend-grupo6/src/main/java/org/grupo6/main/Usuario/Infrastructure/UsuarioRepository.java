37b5eae4ceba5ccb8ef49fbbc5bd141eb4a5661b
y0yoyopi <165915247+y0yoyopi@users.noreply.github.com>
2024-05-26 18:19:45 -0500

    Delete proyecto-backend-grupo6 directory

--- proyecto-backend-grupo6/src/main/java/org/grupo6/main/Usuario/Infrastructure/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package org.grupo6.main.Usuario.Infrastructure;
-
-import org.grupo6.main.domain.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Métodos de búsqueda específicos, si son necesarios
-}
